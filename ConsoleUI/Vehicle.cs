1868e6898ad59184762cb978487a39b8fa120384
Graham Galloway <Grahamgalloway0@gmail.com>
2022-10-05 14:18:26 -0400

    initial commit

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "Toyota";
+        public string Model { get; set; } = "Prius";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive");
+        }
+        
+
+        
+
+
+    }
+}
